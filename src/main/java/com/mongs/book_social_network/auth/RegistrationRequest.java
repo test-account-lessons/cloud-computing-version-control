3927044b9804991e6ea21f95ef01fb8d70429a81
cm-collins <dev-collins@outlook.com>
2025-09-25 19:53:51 +0300

    cloud chnges :

--- src/main/java/com/mongs/book_social_network/auth/RegistrationRequest.java
+++ src/main/java/com/mongs/book_social_network/auth/RegistrationRequest.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+
+// Data Transfer Object for user registration request
+// Includes validation annotations to ensure data integrity
 @Getter
 @Setter
 @RequiredArgsConstructor
